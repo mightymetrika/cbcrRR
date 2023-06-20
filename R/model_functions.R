9ebda97a57e7b2946188e8416ebf0c368227470c
Mackson Ncube <macksonncube@gmail.com>
2023-06-20 15:07:33 +0000

    add run_lcga_models

---
+++ R/model_functions.R
@@ -0,0 +1,28 @@
+run_lcga_models <- function(sim_data, ng = 1) {
+
+  # List to store model results
+  model_outs <- list()
+
+  # Random intercept model w/ no interaction
+  model_1 <- lcmm::hlme(y ~ time + covar, random = ~ 1 | ID, subject = 'ID',
+                        data = sim_data, ng = ng)
+  model_outs$ri_no_int <- model_1
+
+  # Random intercept model w/ interaction
+  model_2 <- lcmm::hlme(y ~ time * covar, random = ~ 1 | ID, subject = 'ID',
+                        data = sim_data, ng = ng)
+  model_outs$ri_int <- model_2
+
+  # Random intercept and slope model w/ no interaction
+  model_3 <- lcmm::hlme(y ~ time + covar, random = ~ time | ID, subject = 'ID',
+                        data = sim_data, ng = ng)
+  model_outs$ris_no_int <- model_3
+
+  # Random intercept and slope model w/ interaction
+  model_4 <- lcmm::hlme(y ~ time * covar, random = ~ time | ID, subject = 'ID',
+                        data = sim_data, ng = ng)
+  model_outs$ris_int <- model_4
+
+  return(model_outs)
+}
+
